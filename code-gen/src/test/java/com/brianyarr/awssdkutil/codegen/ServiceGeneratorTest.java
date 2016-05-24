ad16647bd1692b3c64011653d95acc5b57a66d2e
Brian Yarr <brian.yarr@bgch.co.uk>
2016-05-24 09:09:31 +0100

    Added a test for code generation.

---
+++ code-gen/src/test/java/com/brianyarr/awssdkutil/codegen/ServiceGeneratorTest.java
@@ -0,0 +1,32 @@
+package com.brianyarr.awssdkutil.codegen;
+
+import com.amazonaws.services.lambda.AWSLambda;
+import com.amazonaws.services.lambda.model.ListAliasesRequest;
+import com.amazonaws.services.lambda.model.ListAliasesResult;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Method;
+
+public class ServiceGeneratorTest {
+
+    @Test
+    public void shouldWork() throws NoSuchMethodException {
+        final Class<AWSLambda> serviceInterface = AWSLambda.class;
+        final Method testMethod = serviceInterface.getMethod("listAliases", ListAliasesRequest.class);
+
+        final Method getTokenMethod = ListAliasesResult.class.getMethod("getNextMarker");
+        final Method withTokenMethod = ListAliasesRequest.class.getMethod("withMarker", String.class);
+        final Method resultCollectionMethod = ListAliasesResult.class.getMethod("getAliases");
+
+        final ClassGenerator classGenerator = Mockito.mock(ClassGenerator.class);
+        final ServiceGenerator serviceGenerator = new ServiceGenerator(classGenerator, serviceInterface);
+        serviceGenerator.addMethod(testMethod);
+
+        Mockito.verify(classGenerator).createClass("Lambda", serviceInterface);
+        Mockito.verify(classGenerator).addMethod("listAliases", ListAliasesResult.class, ListAliasesRequest.class, getTokenMethod, withTokenMethod, resultCollectionMethod);
+
+
+    }
+
+}
