4357cbe208d13b887d2436c65502869ec8bcbc0c
Brian Yarr <brian.yarr@bgch.co.uk>
2016-05-25 12:12:56 +0100

    Clean up some of the module gen code.

--- jaws-code-gen/src/main/java/com/brianyarr/jaws/codegen/ModuleGen.java
+++ jaws-code-gen/src/main/java/com/brianyarr/jaws/codegen/ModuleGen.java
@@ -2,12 +2,14 @@ package com.brianyarr.jaws.codegen;
 
 import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.sns.AmazonSNS;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
 import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 public class ModuleGen {
 
@@ -26,15 +28,17 @@ public class ModuleGen {
         final File moduleDir = new File(rootDir, moduleName);
         if (!moduleDir.exists()) {
             moduleDir.mkdir();
-            writeGradleFile(moduleDir, awsModuleName);
-            updateGradleSettings(moduleName);
         }
+
+        writeGradleFile(moduleDir, awsModuleName);
+        updateGradleSettings(moduleName);
 
         final File genSrcDir = new File(moduleDir, "src/gen/java");
         if (genSrcDir.exists()) {
-            genSrcDir.delete();
+            FileUtils.cleanDirectory(genSrcDir);
+        } else {
+            genSrcDir.mkdirs();
         }
-        genSrcDir.mkdirs();
 
         final ServiceGenerator serviceGenerator = new ServiceGenerator(new JavaPoetClassGenerator(genSrcDir), serviceInterface, "com.brianyarr.jaws." + awsModuleName);
         serviceGenerator.tryAddAllMethods();
@@ -42,15 +46,32 @@ public class ModuleGen {
 
     }
 
+    public void cleanModule(final Class<?> serviceInterface) throws IOException {
+        final String awsModuleName = Util.getAwsModuleName(serviceInterface).toLowerCase();
+
+        final String moduleName = "jaws-" + awsModuleName;
+        final File moduleDir = new File(rootDir, moduleName);
+        if (moduleDir.exists()) {
+            FileUtils.cleanDirectory(moduleDir);
+        }
+
+    }
+
     private void updateGradleSettings(final String moduleName) throws IOException {
         final File file = new File(rootDir, "settings.gradle");
-        Files.write(file.toPath(), (", '" + moduleName + "'").getBytes(), APPEND);
+        if (Files.lines(file.toPath()).anyMatch(l -> l.contains(moduleName))) {
+            // already has module name
+        } else {
+            Files.write(file.toPath(), (", '" + moduleName + "'").getBytes(), APPEND);
+        }
+
     }
 
     private void writeGradleFile(final File moduleDir, final String awsModuleName) throws IOException {
         final File file = new File(moduleDir, "build.gradle");
-        file.createNewFile();
-        Files.write(file.toPath(), getGradleFile(awsModuleName).getBytes(), APPEND);
+        if (!file.exists()) {
+            Files.write(file.toPath(), getGradleFile(awsModuleName).getBytes(), CREATE_NEW);
+        }
     }
 
     private String getGradleFile(final String awsModuleName) {
@@ -67,13 +88,16 @@ public class ModuleGen {
                 "    compile 'com.amazonaws:aws-java-sdk-%s'\n" +
                 "    genCompile project(':jaws-core')\n" +
                 "    genCompile 'com.amazonaws:aws-java-sdk-%s'\n" +
-                "}", awsModuleName);
+                "}", awsModuleName, awsModuleName);
     }
 
     public static void main(String[] args) throws IOException {
         final ModuleGen moduleGen = new ModuleGen(new File("/Users/brian.yarr/code/jaws/"));
         moduleGen.generateModule(AWSLambda.class);
         moduleGen.generateModule(AmazonSNS.class);
+
+//        moduleGen.cleanModule(AWSLambda.class);
+//        moduleGen.cleanModule(AmazonSNS.class);
     }
 
 }
