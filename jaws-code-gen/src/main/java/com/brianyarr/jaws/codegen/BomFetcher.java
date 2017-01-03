4ec107efec1d09d6c19e21527bca84f03ac6edc3
Brian Yarr <brian.yarr@bgch.co.uk>
2017-01-03 10:44:55 +0000

    Add a BomFetcher to get list of all aws-java-sdk modules.

---
+++ jaws-code-gen/src/main/java/com/brianyarr/jaws/codegen/BomFetcher.java
@@ -0,0 +1,51 @@
+package com.brianyarr.jaws.codegen;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BomFetcher {
+
+    private static URL url(final String version) throws MalformedURLException {
+        return new URL(String.format("http://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bom/%1$s/aws-java-sdk-bom-%1$s.pom", version));
+    }
+
+    private static List<String> getAllModules(final String version) throws IOException, ParserConfigurationException, SAXException {
+        final List<String> result = new ArrayList<>();
+
+        final URL pom = url(version);
+        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        final Document document = dbf.newDocumentBuilder().parse(pom.openStream());
+        final NodeList deps = document.getElementsByTagName("dependency");
+        for (int i = 0; i < deps.getLength(); i++) {
+            final Element element = (Element) deps.item(i);
+            final String groupId = element.getElementsByTagName("groupId").item(0).getTextContent();
+            if (!groupId.equals("com.amazonaws")) {
+                continue;
+            }
+
+            final String artifactId = element.getElementsByTagName("artifactId").item(0).getTextContent();
+            if (!artifactId.startsWith("aws-java-sdk")) {
+                continue;
+            }
+            result.add(artifactId);
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+        final List<String> modules = getAllModules("1.11.75");
+        System.out.println(modules);
+    }
+
+}
